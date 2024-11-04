8152c5b082ee63d93208c49854da509edbfd9b92
Geovanna-Albuquerque <geovanna.albuquerque1@aluno.ce.gov.br>
2024-11-04 08:58:43 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,12 @@
+# Ler uma lista de 5 números inteiros e
+# mostre cada número juntamente com a
+# sua posição na lista.
+
+listaNumeros = []
+ print ('Informe os 5 numeros')
+ for i in range(5):
+listaNumero.append(input('Numero '+ str(i+1) + ':\n'))
+print (listaNumeros) 
+ 
+
+
